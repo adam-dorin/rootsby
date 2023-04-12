ec1dbafd77cca64f31a75078771bd30f43af731b
Adam <adamoctav@gmail.com>
2023-04-12 16:44:42 +0300

    update route names

--- create-rootsby-app/src/router.js
+++ create-rootsby-app/src/router.js
@@ -10,22 +10,25 @@ function router(app) {
   // Prompts
   app.get("/prompt/list", promptController.getPromptList);
   app.get("/prompt/:name", promptController.getPrompt);
+  app.post("/prompt/:name/execute", promptController.executePrompt);
   app.post("/prompt", promptController.createOrUpdatePrompt);
-  app.delete("/prompt", promptController.deletePrompt);
+  app.delete("/prompt/:name", promptController.deletePrompt);
 
   // Scripts
   app.get("/script/list", scriptsController.getScriptList);
   app.get("/script/:name", scriptsController.getScript);
   app.post("/script", scriptsController.createOrUpdateScript);
   app.delete("/script", scriptsController.deleteScript);
-  app.get("/script/:name/call", scriptsController.callScript);
+  app.get("/script/:name/execute", scriptsController.callScript);
 
   // Workflows
   app.get("/workflow/list", workflowController.getWorkflowList);
   app.get("/workflow/:name", workflowController.getWorkflow);
   app.post("/workflow", workflowController.createOrUpdateWorkflow);
   app.delete("/workflow", workflowController.deleteWorkflow);
-  app.get("/workflow/:name/call", workflowController.callWorkflow);
+  app.get("/workflow/:name/execute", workflowController.callWorkflow);
+  
+  return app;
 }
 
 exports = module.exports = router;
