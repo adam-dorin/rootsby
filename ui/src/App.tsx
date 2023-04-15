499e8e7a418a52075f55989f1adbae7ef7711c4a
Adam <adamoctav@gmail.com>
2023-04-15 03:17:14 +0300

    [ui] add ui project

---
+++ ui/src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (<h1 className="text-3xl font-bold underline bg-slate-600">Hello world!</h1>)
+}
+
+export default App
