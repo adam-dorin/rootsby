6e0f9ac7971ea7c935ac1cffcebc117ea66dab13
Adam <adamoctav@gmail.com>
2023-04-16 06:17:29 +0300

    [ui] add initial version of the horizontal list

--- ui/src/components/hr-list.tsx
+++ ui/src/components/hr-list.tsx
@@ -1,3 +1,22 @@
-export function HrList(){
-    
+import { useState } from 'react';
+export type HrItem = {
+    name: string;
+    description: string;
+}
+
+export function HrList({ list, select }: { list: HrItem[], select?: number }) {
+    const [selected, setSelected] = useState(select||0);
+    return (
+        <>
+            <div className="flex justify-start w-full">
+                <div className="flex items-center space-x-4 w-full">
+                    <div className="tabs tabs-boxed m-0 relative w-full">
+                        {list.map((item: HrItem, index: number) =>
+                            (<span onClick={() => setSelected(index)} key={index} className={index === selected ? 'tab tab-lg tab-lifted tab-active' : 'tab tab-lg tab-lifted'}>{item.name}</span>))
+                        }
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
