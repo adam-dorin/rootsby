b20654f6fc19c6e54b50530db1e04008aea6cec4
Adam <adamoctav@gmail.com>
2023-04-18 02:29:12 +0300

    [api] add sqlite db

--- create-rootsby-app/server.js
+++ create-rootsby-app/server.js
@@ -7,30 +7,32 @@ const marked = require("marked");
 const path = require("path");
 const router = require("./src/router");
 
-require("dotenv").config();
-
-const app = express();
-
-app.engine("md", function (path, options, fn) {
-
-  fs.readFile(path, "utf8", function (err, str) {
-    if (err) return fn(err);
-    var html = marked.parse(str).replace(/\{([^}]+)\}/g, function (_, name) {
-      return escapeHtml(options[name] || "");
+(async function () {
+  require("dotenv").config();
+  await require("./src/db").db();
+  const app = express();
+  
+  app.engine("md", function (path, options, fn) {
+    fs.readFile(path, "utf8", function (err, str) {
+      if (err) return fn(err);
+      var html = marked.parse(str).replace(/\{([^}]+)\}/g, function (_, name) {
+        return escapeHtml(options[name] || "");
+      });
+      fn(null, html);
     });
-    fn(null, html);
   });
-});
 
-app.set("views", path.join(__dirname, "src/views"));
+  app.set("views", path.join(__dirname, "src/views"));
 
-// make it the default, so we don't need .md
-app.set("view engine", "md");
+  // make it the default, so we don't need .md
+  app.set("view engine", "md");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
-router(app);
+  router(app);
 
-app.listen(4353);
-console.log("Express started on port 4353");
+  app.listen(+process.env.PORT);
+  console.log(`Server is running at http://localhost:${process.env.PORT}`);
+})();
+
