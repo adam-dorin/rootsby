b61721191908c67677a3959224fcccb49deeab69
Adam <adamoctav@gmail.com>
2023-04-21 17:48:41 +0300

    [ui] update modal styling

--- ui/src/components/modal.tsx
+++ ui/src/components/modal.tsx
@@ -1,7 +1,6 @@
 import { ReactNode, useEffect, useState } from "react";
 
 export function Modal({ visible, content, actions }: { visible?: boolean, content?: ReactNode, actions?: ReactNode }) {
-    console.log('Modal:::', visible, content);
     const [isVisible, setIsVisible] = useState(false);
     const toggleModalClass = () => isVisible ? 'block' : 'hidden';
     const backdropClass = () => `flex fixed top-0 left-0 w-[100%] h-[100%] backdrop-blur ${toggleModalClass()}`;
@@ -15,11 +14,11 @@ export function Modal({ visible, content, actions }: { visible?: boolean, conten
     return (
       <div className={backdropClass()} onClick={() => setIsVisible(false)}>
         <div className="card w-[50%] h-[50%] fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-neutral text-neutral-content border-2 border-indigo-400" onClick={modalBody}>
-          <div className="card-body items-center text-center">
-            <div className="card-content overflow-auto h-[80%] block w-full">
+          <div className="p-3 h-full text-center">
+            <div className="card-content overflow-y-auto overflow-x-hidden h-[80%] block w-full">
               {content}
             </div>
-            <div className="card-actions justify-end">
+            <div className="card-actions h-[20%] w-full shadow-0 justify-end">
               {actions}
             </div>
           </div>
