ea4adc9cae084c8036338818a1fccdb6cd0925b5
adam-dorin <adamoctav@gmail.com>
2024-09-18 01:25:50 +0300

    rewrite workflow and how it supposed to work

--- src/workflow.ts
+++ src/workflow.ts
@@ -1,139 +1,249 @@
-import { AndAlwaysPush, Observable } from "./observer";
-import { ExpressionParser, PrimitiveExpression } from "./workflow-utils";
+import {  Observable } from "./observer";
+import { ExprOperators, ExpressionParser, PrimitiveExpression } from "./workflow-utils";
 
-export type Operator = "eq" | "not" | "gt" | "lt" | "gt_eq" | "lt_eq";
-export enum ExprOp {
-  eq = "eq",
-  not = "not",
-  gt = "gt",
-  lt = "lt",
-  gt_eq = "gt_eq",
-  lt_eq = "lt_eq",
+export enum WorkflowType {
+  LongRunning = "LongRunning",
+  ShortRunning = "ShortRunning",
 }
-
 type WorkflowCondition = {
-  function_id: string;
-  operator: Operator;
-  value: any;
+  operator: ExprOperators;
+  value: PrimitiveExpression;
 };
 
 type FileLoader = Promise<string>;
+
+type WorkflowMiddlewareFunction = {
+  name: string;
+  executor: (data: unknown) => unknown;
+};
+
+type WorkflowConfig = {
+  id: string;
+  name: string;
+  type: WorkflowType;
+  description?: string;
+  functions: WorkflowFunction[];
+};
 
 type WorkflowFunction = {
   id: string;
   name: string;
   description?: string;
-  file: string | Promise<() => any> | (() => any);
-  next: WorkflowCondition[];
-};
-type WorkflowConfig = {
-  id: string;
-  name: string;
-  description?: string;
-  functions: WorkflowFunction[];
+  middleware?: string[];
+  file?: string | Promise<(data: unknown) => unknown> | ((data: unknown) => unknown);
+  next: WorkflowNextFunction[];
 };
 
-const config: WorkflowConfig = {
-  id: "926cc4ce-36dc-4238-9e59-cae361710973",
-  name: "string",
-  description: "string",
-  functions: [
-    {
-      id: "044ac245-f7b8-4a0f-84ea-ba3dfe494826",
-      name: "string",
-      description: "string",
-      file: "string", // loader
-      next: [
-        {
-          function_id: "bf28b4ed-ca2b-4811-9999-f3f31bdc3e49",
-          operator: "eq",
-          value: "string",
-        },
-      ],
-    },
-    {
-      id: "bf28b4ed-ca2b-4811-9999-f3f31bdc3e49",
-      name: "string",
-      description: "string",
-      file: "string", // loader
-      next: [
-        {
-          function_id: "uuid",
-          operator: "eq",
-          value: "string",
-        },
-      ],
-    },
-  ],
+enum WorkflowEvent {
+  startWorkflow = "startWorkflow",
+  endWorkflow = "endWorkflow",
+  startStep = "startStep",
+  endStep = "endStep",
+  beforeMiddleware = "beforeMiddleware",
+  afterMiddleware = "afterMiddleware",
+  error = "error",
+}
+
+type WorkflowNextFunction = {
+  functionId: string;
+  values: WorkflowCondition[];
+};
+type WorkflowFunctionsValidationResult = {
+  error: boolean;
+  results: WorkflowNextFunction[];
 };
 
-function loadFile(file: string): Promise<(input?: any) => any> {
+
+/**
+ *
+ * Need to cover 2 cases for the workflow:
+ *
+ * 1. The Workflow is a long running workflow
+ *
+ * When the workflow is a long running workflow,
+ * the workflow will be started and will stop and output its current state and step step.
+ * The workflow will be started again from the last step.
+ *
+ * 2. The Workflow is a short running workflow
+ * When the workflow is a short running workflow,
+ * the workflow will be started and will run from function to function until the end.
+ *
+ */
+export class Rootsby {
+  private functionMap: Map<string, WorkflowFunction>;
+  private middlewareFunctionMap: Map<string, WorkflowMiddlewareFunction>;
+  private eventBus: Observable<unknown>;
+  private config?: WorkflowConfig;
+
+  constructor(config?: { middlewareFunctions?: WorkflowMiddlewareFunction[]; logger?: (name: string, data: unknown) => void }) {
+    this.functionMap = new Map();
+    this.middlewareFunctionMap = new Map();
+    this.eventBus = new Observable<unknown>(false);
+
+    if (config && config.middlewareFunctions) {
+      this.setMiddlewareConfig(config.middlewareFunctions);
+    }
+
+    if (config && config.logger) {
+      this.setLogger(config.logger);
+    }
+  }
+
+  private setMiddlewareConfig(middlewareFunctions: WorkflowMiddlewareFunction[] = []) {
+    this.middlewareFunctionMap = new Map();
+    middlewareFunctions.forEach((fn) => {
+      this.middlewareFunctionMap.set(fn.name, fn);
+    });
+  }
+  private setLogger(logger: (name: string, data: unknown) => void) {
+    this.eventBus.setLogger(logger);
+  }
+
+  private endWorkflow(data?: { functionId: string; data: unknown }) {
+    if (data) {
+      this.eventBus.push(WorkflowEvent.endWorkflow, data);
+    }
+    this.config?.functions.forEach((fn) => {
+      this.eventBus.unsubscribeAll(fn.id);
+    });
+  }
+
+  private logErrorAndEndWorkflow(data: { functionId: string; data: unknown; error: string }): void {
+    this.eventBus.push(WorkflowEvent.error, data);
+    this.endWorkflow();
+  }
+
+  private validateNextFunctions(result: PrimitiveExpression, nextFunctions: WorkflowNextFunction[]): WorkflowFunctionsValidationResult {
+    if (typeof result !== "string" && typeof result !== "number" && typeof result !== "boolean") {
+      return { error: true, results: [] };
+    }
+    const next = nextFunctions.filter((workflowNextFunction: WorkflowNextFunction) => {
+      return workflowNextFunction.values
+        .map((condition: WorkflowCondition) => {
+          return ExpressionParser.run(result as PrimitiveExpression, condition.value, condition.operator);
+        })
+        .every((value: boolean) => value === true);
+    });
+    return { error: false, results: next };
+  }
+
+  private advanceToNextFunction(functionId: string, result: unknown, nextValidFunctions: WorkflowNextFunction[]): void {
+    const workflowFunction = this.functionMap.get(functionId);
+    if (!workflowFunction) {
+      throw new Error(`Function with id ${functionId} not found`);
+    }
+
+    for (const nextFunction of nextValidFunctions) {
+      this.eventBus.push(nextFunction.functionId, result);
+    }
+  }
+
+  private runMiddlewareFunctions(middlewareFunctionNames: string[], data: unknown): unknown {
+    let result: unknown = data;
+    for (const middlewareFunctionName of middlewareFunctionNames) {
+      const middlewareFunction = this.middlewareFunctionMap.get(middlewareFunctionName);
+      if (!middlewareFunction) {
+        throw new Error(`Middleware function ${middlewareFunctionName} not found. Check if \`setMiddlewareConfig\` was called`);
+      }
+      this.eventBus.push(WorkflowEvent.beforeMiddleware, { middlewareFunctionName, data: result });
+      result = middlewareFunction.executor(result);
+      this.eventBus.push(WorkflowEvent.afterMiddleware, { middlewareFunctionName, data: result });
+    }
+    return result;
+  }
+
+  private eventBusSubscriber(functionId: string): (data: unknown) => void {
+    return async (data: unknown) => {
+      this.eventBus.push(WorkflowEvent.startStep, { functionId, data: data });
+      let result: unknown = data;
+
+      const workflowFunction = this.functionMap.get(functionId);
+      if (!workflowFunction) {
+        throw new Error(`Function with id ${functionId} not found`);
+      }
+
+      if (workflowFunction.middleware && workflowFunction.middleware.length > 0) {
+        result = this.runMiddlewareFunctions(workflowFunction.middleware, result);
+      }
+
+      if (workflowFunction.file && typeof workflowFunction.file === "function") {
+        // TODO: strengthen the type here
+        result = (workflowFunction.file as (data: unknown) => unknown)(data);
+      }
+
+      const nextValid = this.validateNextFunctions(result as PrimitiveExpression, workflowFunction.next);
+      this.eventBus.push(WorkflowEvent.endStep, { functionId, data: result });
+      if (this.config && this.config.type === WorkflowType.LongRunning) {
+        if (nextValid.error) {
+          // throw new Error("Invalid result type");
+          // TODO: make this error more descriptive
+          this.logErrorAndEndWorkflow({ functionId, data: result, error: "Invalid result type" });
+        }
+        // output the current state and step, then end the workflow
+        this.endWorkflow({ functionId, data: result });
+      }
+
+      if (this.config && this.config.type === WorkflowType.ShortRunning) {
+        if (nextValid.error) {
+          // throw new Error("Invalid result type");
+          // TODO: make this error more descriptive
+          this.logErrorAndEndWorkflow({ functionId, data: result, error: "Invalid result type" });
+        }
+        if (nextValid.results.length) {
+          this.advanceToNextFunction(functionId, result, nextValid.results);
+        } else {
+          // output the current state and step, then end the workflow
+          this.endWorkflow({ functionId, data: result });
+        }
+      }
+    };
+  }
+
+  // TODO: add a type for the file loader | acctually make it work
+  private async loadFile(file: string): Promise<(input?: any) => any> {
     return new Promise((resolve, reject) => {
       resolve(() => "world");
     });
   }
-/**
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- */
-export async function runWorkflow(config: WorkflowConfig) {
-  // config.functions.forEach((fn) => {
-  const functionMap: { [name: string]: WorkflowFunction } = {};
-  const eventBus = new Observable(false);
-  let currentId;
 
-  for (let i = 0; i < config.functions.length; i++) {
-    const functionId = config.functions[i].id;
-    functionMap[functionId] = config.functions[i];
-    functionMap[functionId].file = await loadFile(functionMap[functionId].file as string);
-    // fn.id
-    if (!currentId) {
-      currentId = functionId;
-    }
-    eventBus.subscribe(functionId, async (data) => {
-      // const fileFunction = await loadFile(functionMap[functionId].file as string);
-      if (typeof functionMap[functionId].file === "function") {
-        // eslint-disable-next-line @typescript-eslint/ban-types
-        const result = (functionMap[functionId].file as Function)(data);
-        
-        const next =
-        config.functions[i].next.filter((condition: WorkflowCondition) => {
-          return ExpressionParser.run(result as PrimitiveExpression, condition.value, condition.operator);
-        });
-        if (next.length > 0) {
-            for (let j = 0; j < next.length; j++) {
-                eventBus.push(next[j].function_id, result);
-            }
-        }
+  public async runWorkflow(config: WorkflowConfig, data?: { currentStepId?: string; currentStepData?: unknown }): Promise<void> {
+    this.config = config;
+    this.eventBus.push(WorkflowEvent.startWorkflow, { config, data });
+    for (const workflowFunction of config.functions) {
+      const functionId = workflowFunction.id;
+      if (workflowFunction.file) {
+        workflowFunction.file = await this.loadFile(workflowFunction.file as string);
       }
-    });
+
+      this.functionMap.set(functionId, workflowFunction);
+      const subscriber = this.eventBusSubscriber(functionId);
+      this.eventBus.subscribe(functionId, subscriber);
+    }
+
+    if (!data) {
+      console.log("pushing to first function [1]");
+      this.eventBus.push(config.functions[0].id, null);
+    }
+    if (data && !data.currentStepId) {
+      console.log("pushing to first function [2]");
+      this.eventBus.push(config.functions[0].id, data.currentStepData);
+    }
+    if (data && data.currentStepId) {
+      console.log("pushing to first function [3]");
+      this.eventBus.push(data.currentStepId, data.currentStepData);
+    }
   }
-  return {
-    start: (data: any) => {
-      eventBus.push(config.functions[0].id, data);
-    },
-    end: () => {
-      eventBus.unsubscribeAll(AndAlwaysPush);
-      config.functions.forEach((fn) => {
-        eventBus.unsubscribeAll(fn.id);
-      });
-    },
-    pushLog: (logger: (data?: any) => void) => {
-      eventBus.subscribe(AndAlwaysPush, logger);
-    },
-  };
+
+  public on(eventName: WorkflowEvent, callback: (data: unknown) => void) {
+    this.eventBus.subscribe(eventName, callback);
+  }
 }
 
 // a workflowConfig example with a single function and valid uuids
 const config_: WorkflowConfig = {
   id: "uuid",
   name: "string",
+  type: WorkflowType.ShortRunning,
   description: "string",
   functions: [
     {
@@ -141,11 +251,16 @@ const config_: WorkflowConfig = {
       name: "string",
       description: "string",
       file: "string", // loader
+      middleware: ["middleman"],
       next: [
         {
-          function_id: "uuid",
-          operator: "eq",
-          value: "string",
+          functionId: "uuid",
+          values: [
+            {
+              operator: "eq",
+              value: "string",
+            },
+          ],
         },
       ],
     },
@@ -153,10 +268,86 @@ const config_: WorkflowConfig = {
 };
 
 async function main() {
-  const wf = await runWorkflow(config_)
-  wf.pushLog((data) => {
-    console.log('log::::',data);
+  const config: WorkflowConfig = {
+    id: "926cc4ce-36dc-4238-9e59-cae361710973",
+    name: "string",
+    type: WorkflowType.ShortRunning,
+    description: "string",
+    functions: [
+      {
+        id: "044ac245-f7b8-4a0f-84ea-ba3dfe494826",
+        name: "string",
+        description: "string",
+        file: "string", // loader
+        middleware: ["middleman"],
+        next: [
+          {
+            functionId: "bf28b4ed-ca2b-4811-9999-f3f31bdc3e49",
+            values: [
+              {
+                operator: "eq",
+                value: "world",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        id: "bf28b4ed-ca2b-4811-9999-f3f31bdc3e49",
+        name: "string",
+        description: "string",
+        file: "string", // loader
+        middleware: ["middleman"],
+        next: [
+          {
+            functionId: "uuid",
+            values: [
+              {
+                operator: "eq",
+                value: "string",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  };
+  const rootsby = new Rootsby({
+    middlewareFunctions: [
+      {
+        name: "middleman",
+        executor: (data: unknown) => {
+          console.log("middleman:", data);
+          return data;
+        },
+      },
+    ],
+    logger: (name, data) => {
+      console.log("logger:>>>>", name, data);
+    },
   });
-  wf.start("hello");
+  rootsby.on(WorkflowEvent.startWorkflow, (data) => {
+    console.log("startWorkflow", data);
+  });
+  rootsby.on(WorkflowEvent.endWorkflow, (data) => {
+    console.log("endWorkflow", data);
+  });
+  rootsby.on(WorkflowEvent.startStep, (data) => {
+    console.log("startStep", data);
+  });
+  rootsby.on(WorkflowEvent.endStep, (data) => {
+    console.log("endStep", data);
+  });
+  rootsby.on(WorkflowEvent.beforeMiddleware, (data) => {
+    console.log("beforeMiddleware", data);
+  });
+  rootsby.on(WorkflowEvent.afterMiddleware, (data) => {
+    console.log("afterMiddleware", data);
+  });
+  rootsby.on(WorkflowEvent.error, (data) => {
+    console.log("error", data);
+  });
+
+  await rootsby.runWorkflow(config, { currentStepData: "hello" });
 }
 main();
